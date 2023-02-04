a4b2273ff8b595238514eca79dff0376ff633cde
annaberezko <site.vizit@gmail.com>
2023-02-04 17:41:02 +0200

    startapp events add models Event and EventType Add events to settings INSTALLED_APPS

---
+++ events/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+User = get_user_model()
+
+
+class EventType(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_type = models.ForeignKey(EventType, on_delete=models.CASCADE)
+    info = models.JSONField()
+    timestamp = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
